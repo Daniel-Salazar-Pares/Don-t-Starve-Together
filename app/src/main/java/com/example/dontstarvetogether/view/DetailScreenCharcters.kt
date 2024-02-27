188c0252f2b581e56050ab6392542bbaf7a4b2ff
Toumai <daniel.salazar.7e7@itb.cat>
2024-02-27 01:39:23 +0100

    detail screen ceration (not finish) && scroll screen finish

--- app/src/main/java/com/example/dontstarvetogether/view/DetailScreenCharcters.kt
+++ app/src/main/java/com/example/dontstarvetogether/view/DetailScreenCharcters.kt
@@ -1,23 +1,25 @@
 package com.example.dontstarvetogether.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.dontstarvetogether.model.character.Data
 import com.example.dontstarvetogether.viewmodel.APIViewModel
-import java.lang.reflect.Modifier
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.ui.layout.ContentScale
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun DetailScreenCharacters(navController: NavController, characterName: String, myViewModel: APIViewModel) {
     val characterDetails by myViewModel.characters.observeAsState(initial = Data())
@@ -26,10 +28,20 @@ fun DetailScreenCharacters(navController: NavController, characterName: String, 
     if (characterDetails.isNotEmpty()) {
         val character = characterDetails.find { it.name == characterName }
         if (character != null) {
-            // Detail view for a character
-            Column() {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
                 Text(
                     text = character.name,
+                )
+
+                GlideImage(
+                    model = character.portrait,
+                    contentDescription = "Character Portrait",
+                    contentScale = ContentScale.Crop,
+                    modifier = androidx.compose.ui.Modifier.size(100.dp)
                 )
                 Text(text = "Nickname: ${character.nickname}")
                 Text(text = "Quote: \"${character.quote}\"")
